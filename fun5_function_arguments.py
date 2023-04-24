c4b57f90f5abf2434ab235f6b7f3e806e175de74
Satyam_jaysawal <sjrecm9258@gmail.com>
2023-04-24 21:51:18 +0530

    second commit

---
+++ fun5_function_arguments.py
@@ -0,0 +1,12 @@
+def say_hello(name):
+    print(f'Hello {name}')
+    print("{} is name".format(name))
+
+say_hello('Carlos')
+# Hello Carlos
+
+say_hello('Wanda')
+# Hello Wanda
+
+say_hello('Rose')
+# Hello Rose
